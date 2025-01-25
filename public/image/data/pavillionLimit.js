72fe63b0cc9ca13b1ba424abab56d17e8c7f83cc
Rashim <rashimrb22@gmail.com>
2025-01-25 15:35:23 +0530

    Refactor login and patrol attendance routes for improved functionality and add pavilion limits

---
+++ public/image/data/pavillionLimit.js
@@ -0,0 +1,23 @@
+export const pavillionLimits = [
+    {
+        "People": 2
+    },
+    {
+        "Prosperity": 2
+    },
+    {
+        "Planet": 2
+    },
+    {
+        "Peace and Partnership": 2
+    },
+    {
+        "WAGGGS": 4
+    },
+    {
+        "WOSM": 12
+    },
+    {
+        "CLAP": 7
+    },
+]
